c3ce0ad708847f882cf226287381acd5485d27ec
sponbob-pat <ixdos1111@gmail.com>
2023-03-13 16:43:15 +0900

    유연한 컨트롤러(V4 적용) - V5

--- src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
@@ -1,4 +1,4 @@
-package hello.servlet.web.frontcontroller.v5.adater;
+package hello.servlet.web.frontcontroller.v5.adapter;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -9,26 +9,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import hello.servlet.web.frontcontroller.ModelView;
-import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
 import hello.servlet.web.frontcontroller.v5.MyHandlerAdapter;
 
-public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+public class ControllerV4HandlerAdapter implements MyHandlerAdapter {
 
     @Override
     public boolean supports(Object handler) {
-        return (handler instanceof ControllerV3);
+        return (handler instanceof ControllerV4);
     }
 
     @Override
     public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+        ControllerV4 controller = (ControllerV4) handler;
 
-        // 4. handler 호출
-        ControllerV3 controller = (ControllerV3) handler;
-
+        // V4의 frontcontroller의 service 부분과 유사함
         Map<String, String> paramMap = createParamMap(request);
-        ModelView mv = controller.process(paramMap);
+        HashMap<String, Object> model = new HashMap<>();
+
+        String viewName = controller.process(paramMap, model);
+
+        ModelView mv = new ModelView(viewName);
+        mv.setModel(model);
 
         return mv;
+
     }
 
     private Map<String, String> createParamMap(HttpServletRequest request) {
